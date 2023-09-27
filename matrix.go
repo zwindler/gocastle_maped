9a12ff52bde25dc096560edda54f4b7da44ed09a
Denis GERMAIN <dt.germain@gmail.com>
2023-09-27 14:10:26 +0200

    feat: add button to print json

--- matrix.go
+++ matrix.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
@@ -8,14 +11,51 @@ import (
 )
 
 func showMatrixScreen(window fyne.Window, columns, rows int) {
-	mainContent := container.New(layout.NewGridLayoutWithColumns(columns))
-	for x := 0; x < columns; x++ {
-		for y := 0; y < rows; y++ {
+	var entriesMatrix [][]*widget.Entry
+
+	mainGrid := container.New(layout.NewGridLayoutWithColumns(columns))
+	for y := 0; y < rows; y++ {
+		currentRow := make([]*widget.Entry, columns)
+		for x := 0; x < columns; x++ {
 			input := widget.NewEntry()
 			input.SetPlaceHolder("0")
-			mainContent.Add(input)	
+			currentRow[x] = input
+			mainGrid.Add(input)
 		}
+		entriesMatrix = append(entriesMatrix, currentRow)
 	}
+
+	resetButton := widget.NewButton("Reset Matrix", func() {
+		showMatrixScreen(window, columns, rows)
+	})
+
+	previewButton := widget.NewButton("Preview", func() {
+
+	})
+
+	validateButton := widget.NewButton("Generate JSON", func() {
+		var matrix [][]string
+		for y := 0; y < rows; y++ {
+			row := make([]string, columns)
+			for x := 0; x < columns; x++ {
+				value := entriesMatrix[y][x].Text
+				if value == "" {
+					value = "0"
+				}
+				row[x] = value
+			}
+			matrix = append(matrix, row)
+		}
+		jsonData, err := json.Marshal(matrix)
+		if err != nil {
+			fmt.Println("Error encoding JSON:", err)
+			return
+		}
+		fmt.Println(string(jsonData))
+	})
+	lastLine := container.New(layout.NewGridLayoutWithColumns(3), resetButton, previewButton, validateButton)
+
+	mainContent := container.NewBorder(nil, lastLine, nil, nil, mainGrid)
 
 	window.SetContent(mainContent)
 }
